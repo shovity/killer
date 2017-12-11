3ed87640320ec460ce965bf6f789971996301951
shovity <shovity@gmail.com>
2017-12-11 22:12:09 +0700

    fix inject css, update venom exist

--- injectContent.js
+++ injectContent.js
@@ -1,18 +1,15 @@
 window.killing = () => {
   const deadString = localStorage.deadString;
   // check exits dead string
-  if (!localStorage.deadString) return
-
-  // inject css
-  document.getElementsByTagName('head')[0] += `
-    <style>
-      ${deadString} {
-        display: none;
-        opacity: 0;
-        pointer-events: none;
-      }
-    </style>
-  `
+  if (!localStorage.deadString) return;
+  const cssInject = `<style>${deadString} {display:none;opacity:0;pointer-events:none;}</style>`;
+  const venom = document.getElementById('venom');
+  
+  if (venom) {
+    venom.innerHTML = cssInject;
+  } else {
+    document.getElementsByTagName('head')[0].innerHTML += cssInject;
+  }
 }
 
 // killing...
