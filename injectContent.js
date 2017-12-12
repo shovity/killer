685daad82e319cb2093afd203057e1ee1d444f4a
shovity <shovity@gmail.com>
2017-12-12 16:07:20 +0700

    add !important css

--- injectContent.js
+++ injectContent.js
@@ -2,7 +2,15 @@ window.killing = () => {
   const deadString = localStorage.deadString;
   // check exits dead string
   if (!localStorage.deadString) return;
-  const cssInject = `<style>${deadString} {display:none;opacity:0;pointer-events:none;}</style>`;
+  const cssInject = `
+    <style>
+      ${deadString} {
+        display: none !important;
+        opacity:0 !important;
+        pointer-events: none !important;
+      }
+    </style>
+  `;
   const venom = document.getElementById('venom');
   
   if (venom) {
