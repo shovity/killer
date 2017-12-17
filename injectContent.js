05de558508726646685d5d18d1cc6e474ab1647f
Huong Nguyen <shovity@gmail.com>
2017-12-17 19:41:15 +0700

    fix linux platform

--- injectContent.js
+++ injectContent.js
@@ -1,24 +1,30 @@
-window.killing = () => {
+window.killing = (options = {}) => {
+
+  const venom = document.getElementById('venom');
+  
+  if (options.clean === true && venom) {
+    venom.innerHTML = '';
+    return
+  }
+
   const deadString = localStorage.deadString;
   // check exits dead string
   if (!localStorage.deadString) return;
   const cssInject = `
-    <style>
-      ${deadString} {
-        display: none !important;
-        opacity:0 !important;
-        pointer-events: none !important;
-      }
-    </style>
+   ${deadString} {
+      display: none !important;
+      opacity:0 !important;
+      pointer-events: none !important;
+    }
   `;
-  const venom = document.getElementById('venom');
+
   
   if (venom) {
     venom.innerHTML = cssInject;
   } else {
-    document.getElementsByTagName('head')[0].innerHTML += cssInject;
+    document.getElementsByTagName('head')[0].innerHTML += `<style id="venom">${cssInject}</style>`;
   }
 }
 
 // killing...
-killing()
+killing();
